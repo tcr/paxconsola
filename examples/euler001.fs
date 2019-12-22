b70044a223f1d845ac64deeede396a10e0b09b91
Tim Ryan <id@timryan.org>
2019-12-21 19:39:46 -0500

    Unwriting the r> and >r part from euler001

--- examples/euler001.fs
+++ examples/euler001.fs
@@ -4,17 +4,18 @@
 we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000. )
 
-: temp@ 0 @ ;
-: temp! 0 ! ;
+: temp@ r@ ;
+: >temp dup >r 0 ! ;
+: temp> r> ;
 
 : r@ r> dup >r ;
 
 : g if r@ + else then ;
 
-: f r@ 0 == if else
-  r@ 3 % 0 ==
-  r@ 5 % 0 ==
+: f temp@ 0 == if else
+  temp@ 3 % 0 ==
+  temp@ 5 % 0 ==
   or g
-  r> 1 - >r recurse then ;
-231 3 0 0 push1 >r 0 f print
+  temp> 1 - >temp recurse then ;
+231 3 0 0 push1 >temp 0 f print
 ( 9 >r 0 f print )
