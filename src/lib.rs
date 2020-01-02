403545e02629ebb7517720924ab50fcecf705668
Tim Ryan <id@timryan.org>
2020-01-01 23:57:44 -0500

    Splits out tokenizer

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,11 @@ pub mod parse;
 pub mod ir;
 pub mod eval;
 pub mod location;
+pub mod tokenizer;
 
 pub use location::*;
 pub use ir::*;
 pub use compile::*;
 pub use parse::*;
 pub use eval::*;
+pub use tokenizer::*;
--- src/parse.rs
+++ src/parse.rs
@@ -3,90 +3,6 @@ use indexmap::IndexMap;
 use lazy_static::*;
 use regex::Regex;
 
-#[derive(Debug, Clone, PartialEq)]
-pub enum Token {
-    Word(String),
-    Literal(isize),
-}
-
-#[derive(Debug, Clone)]
-pub struct Parser {
-    pub code: String,
-    pub pos: Pos,
-}
-
-impl Parser {
-    pub fn new(code: &str) -> Parser {
-        Parser {
-            code: code.to_string(),
-            pos: Pos {
-                line: 1,
-                col: 1,
-            }
-        }
-    }
-
-    pub fn move_forward(&mut self, length: usize) -> Pos {
-        for c in self.code[..length].chars() {
-            if c == '\n' {
-                self.pos.line += 1;
-                self.pos.col = 1;
-            } else {
-                self.pos.col += 1;
-            }
-        }
-        self.code = self.code[length..].to_string();
-        self.pos.clone()
-    }
-}
-
-impl Iterator for Parser {
-    type Item = Located<Token>;
-
-    fn next(&mut self) -> Option<Located<Token>> {
-        lazy_static! {
-            static ref RE_INT: Regex =
-                Regex::new(r"(?s)^(\-?\$[a-fA-F0-9]+|\-?\d+)(\s+.*?)?$").unwrap();
-            static ref RE_WORD: Regex = Regex::new(r"(?s)^(\S*)(\s+.*?)?$").unwrap();
-        }
-
-        let code_input = self.code.trim_start().to_string();
-        if code_input.is_empty() {
-            return None;
-        }
-        let pos_at = self.move_forward(self.code.len() - code_input.len());
-
-        let matches = RE_INT.captures(&code_input);
-        match matches {
-            Some(cap) => {
-                self.move_forward(self.code.len() - cap[2].len());
-
-                if let Some(index) = cap[1].find("$") {
-                    return Some((Token::Literal(
-                        isize::from_str_radix(&cap[1][index + 1..], 16).unwrap(),
-                    ), pos_at));
-                } else {
-                    return Some((Token::Literal(
-                        cap[1].parse::<isize>().unwrap(),
-                    ), pos_at));
-                }
-            }
-            _ => {}
-        }
-
-        let matches = RE_WORD.captures(&code_input);
-        match matches {
-            Some(cap) => {
-                self.move_forward(self.code.len() - cap[2].len());
-                return Some((Token::Word(cap[1].to_string()), pos_at));
-            }
-            _ => {}
-        }
-
-        panic!("incomplete parse: {:?}", &code_input);
-    }
-}
-
 pub enum ParseMode {
     Default,
     FunctionName,
@@ -136,7 +52,7 @@ pub fn parse_forth(buffer: Vec<u8>) -> Vec<Located<Pax>> {
     // Strip comments
     code = RE_COMMENTS.replace_all(&code, "").to_string();
 
-    let parser = Parser::new(&code);
+    let parser = Tokenizer::new(&code);
 
     let mut output: Vec<Located<Pax>> = vec![];
     let mut constants: IndexMap<String, isize> = IndexMap::new(); // only u16 literals
