4c93097e6dddf411d42de3b53ce0e81df104ffb0
Tim Ryan <trim@fb.com>
2020-05-02 23:29:46 -0400

    Adds desktop module

---
+++ desktop/src/main.rs
@@ -0,0 +1,97 @@
+use ggez;
+use ggez::event;
+use ggez::graphics;
+use ggez::graphics::DrawParam;
+use ggez::nalgebra as na;
+use ggez::timer;
+use paxconsola::wasm::wasmtime::*;
+use paxconsola::*;
+
+struct MainState {
+    pos_x: f32,
+    runner: WasmContext,
+}
+
+impl MainState {
+    fn new() -> ggez::GameResult<MainState> {
+        let src = std::fs::read_to_string("snake.pax").unwrap();
+
+        // Parse program into Pax.
+        let source_program = parse_to_superpax(src.as_bytes().to_owned(), Some("snake.pax"));
+
+        // Main must be inlined before evaluating in WebAssembly.
+        let mut program = source_program.clone();
+        inline_into_function(&mut program, "main");
+        // optimize_function(&mut program, "main");
+        let wasm = cross_compile_forth_wasm(&program);
+
+        let mut runner = parse_wasm(wasm.as_slice(), true).unwrap();
+
+        let s = MainState { pos_x: 0.0, runner };
+
+        Ok(s)
+    }
+}
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
+        const DESIRED_FPS: u32 = 10;
+
+        while timer::check_update_time(ctx, DESIRED_FPS) {
+            self.pos_x = self.pos_x % 800.0 + 1.0;
+
+            self.runner.run().unwrap();
+            let buffer = self.runner.flush_buffer().unwrap();
+            eprint!("{}", String::from_utf8_lossy(&buffer));
+        }
+
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut ggez::Context) -> ggez::GameResult {
+        graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
+
+        const SWIDTH: f32 = 800.0;
+        const SHEIGHT: f32 = 600.0;
+
+        const WIDTH: usize = 20;
+        const HEIGHT: usize = 18;
+
+        const ROW_SKIP: usize = 32;
+
+        for i in 0..WIDTH {
+            for j in 0..HEIGHT {
+                let wunit = SWIDTH / WIDTH as f32;
+                let hunit = SHEIGHT / HEIGHT as f32;
+
+                let status = self
+                    .runner
+                    .get_mem((0x9800 + i + (j * ROW_SKIP)) as u32)
+                    .unwrap();
+
+                // Create and draw a filled rectangle mesh.
+                let circle = graphics::Mesh::new_rectangle(
+                    ctx,
+                    graphics::DrawMode::fill(),
+                    graphics::Rect::new(wunit * i as f32, hunit * j as f32, wunit, hunit),
+                    if status > 0 {
+                        graphics::BLACK
+                    } else {
+                        graphics::WHITE
+                    },
+                )?;
+                graphics::draw(ctx, &circle, DrawParam::default())?;
+            }
+        }
+
+        graphics::present(ctx)?;
+        Ok(())
+    }
+}
+
+pub fn main() -> ggez::GameResult {
+    let cb = ggez::ContextBuilder::new("paxconsola_desktop", "trim");
+    let (ctx, event_loop) = &mut cb.build()?;
+    let state = &mut MainState::new()?;
+    event::run(ctx, event_loop, state)
+}
--- src/wasm/wasmtime.rs
+++ src/wasm/wasmtime.rs
@@ -4,90 +4,122 @@ use std::rc::Rc;
 use std::sync::Mutex;
 use wasmtime::*;
 
+pub struct WasmContext {
+    instance: Instance,
+    buffer: Rc<Mutex<Vec<u8>>>,
+    memory: Rc<Mutex<HashMap<u32, u16>>>,
+}
+
+impl WasmContext {
+    pub fn run(&mut self) -> anyhow::Result<()> {
+        // Next we poke around a bit to extract the `run` function from the module.
+        let run = self
+            .instance
+            .get_export("main")
+            .and_then(|e| e.func())
+            .ok_or(anyhow::format_err!("failed to find `run` function export"))?
+            .get0::<i32>()?;
+
+        run()?;
+
+        Ok(())
+    }
+
+    pub fn flush_buffer(&mut self) -> anyhow::Result<Vec<u8>> {
+        let buffer_ref = self.buffer.lock().unwrap();
+        Ok(buffer_ref.iter().cloned().collect::<Vec<u8>>())
+    }
+
+    pub fn get_mem(&self, index: u32) -> anyhow::Result<u16> {
+        let memory_ref = self.memory.lock().unwrap();
+        Ok(memory_ref.get(&index).map(|x| *x).unwrap_or(0))
+    }
+}
+
+pub fn parse_wasm(binary: &[u8], export_buffer: bool) -> anyhow::Result<WasmContext> {
+    let store = Store::default();
+    let module = Module::from_binary(&store, binary)?;
+
+    // Imports.
+    let buffer: Rc<Mutex<Vec<u8>>> = Rc::new(Mutex::new(Vec::new()));
+    let memory: Rc<Mutex<HashMap<u32, u16>>> = Rc::new(Mutex::new(HashMap::new()));
+    let print_func = Func::wrap(&store, {
+        let buffer = buffer.clone();
+        move |value: i32| -> i32 {
+            if export_buffer {
+                writeln!(&mut *buffer.lock().unwrap(), "{}", value).ok();
+            } else {
+                println!("{}", value);
+            }
+            value
+        }
+    });
+    let extmem_load_func = Func::wrap(&store, {
+        let memory = memory.clone();
+        move |address: i32| -> i32 {
+            let address = (address as u32) & 0xFFFF;
+            *memory.lock().unwrap().get(&address).unwrap_or(&0) as _
+        }
+    });
+    let extmem_load_8_func = Func::wrap(&store, {
+        let memory = memory.clone();
+        move |address: i32| -> i32 {
+            let address = (address as u32) & 0xFFFF;
+            *memory.lock().unwrap().get(&address).unwrap_or(&0) as u8 as _
+        }
+    });
+    let extmem_store_func = Func::wrap(&store, {
+        let memory = memory.clone();
+        move |address: i32, value: i32| -> () {
+            let address = (address as u32) & 0xFFFF;
+            let value = (value as u32) & 0xFFFF;
+            memory.lock().unwrap().insert(address, value as u16);
+        }
+    });
+    let extmem_store_8_func = Func::wrap(&store, {
+        let memory = memory.clone();
+        move |address: i32, value: i32| -> () {
+            let address = (address as u32) & 0xFFFF;
+            let value = (value as u32) & 0xFFFF;
+            memory.lock().unwrap().insert(address, value as u8 as u16);
+        }
+    });
+
+    /*
+    (type $t0 (func))
+    (type $t1 (func (param i32 i32)))
+    (type $t2 (func (param i32) (result i32)))
+    (type $t3 (func (param i32)))
+    (type $t4 (func (result i32)))
+
+    (import "root" "print" (func $print (type $t2)))
+    (import "root" "extmem_load" (func $extmem_load (param i32) (result i32)))
+    (import "root" "extmem_load_8" (func $extmem_load_8 (param i32) (result i32)))
+    (import "root" "extmem_store" (func $extmem_store (param i32) (param i32)))
+    (import "root" "extmem_store_8" (func $extmem_store_8 (param i32) (param i32)))
+    */
+    let imports = [
+        print_func.into(),
+        extmem_load_func.into(),
+        extmem_load_8_func.into(),
+        extmem_store_func.into(),
+        extmem_store_8_func.into(),
+    ];
+
+    // Once we've got that all set up we can then move to the instantiation
+    // phase, pairing together a compiled module as well as a set of imports.
+    // Note that this is where the wasm `start` function, if any, would run.
+    let instance = Instance::new(&module, &imports)?;
+
+    Ok(WasmContext {
+        instance,
+        buffer,
+        memory,
+    })
+}
+
 pub fn run_wasm(binary: &[u8], export_buffer: bool) -> anyhow::Result<Vec<u8>> {
-  let store = Store::default();
-  let module = Module::from_binary(&store, binary)?;
-
-  // Imports.
-  let buffer: Rc<Mutex<Vec<u8>>> = Rc::new(Mutex::new(Vec::new()));
-  let memory: Rc<Mutex<HashMap<u32, u16>>> = Rc::new(Mutex::new(HashMap::new()));
-  let print_func = Func::wrap(&store, {
-    let buffer = buffer.clone();
-    move |value: i32| -> i32 {
-      if export_buffer {
-        writeln!(&mut *buffer.lock().unwrap(), "{}", value).unwrap();
-      } else {
-        println!("{}", value);
-      }
-      value
-    }
-  });
-  let extmem_load_func = Func::wrap(&store, {
-    let memory = memory.clone();
-    move |address: i32| -> i32 {
-      let address = (address as u32) & 0xFFFF;
-      *memory.lock().unwrap().get(&address).unwrap_or(&0) as _
-    }
-  });
-  let extmem_load_8_func = Func::wrap(&store, {
-    let memory = memory.clone();
-    move |address: i32| -> i32 {
-      let address = (address as u32) & 0xFFFF;
-      *memory.lock().unwrap().get(&address).unwrap_or(&0) as u8 as _
-    }
-  });
-  let extmem_store_func = Func::wrap(&store, {
-    let memory = memory.clone();
-    move |address: i32, value: i32| -> () {
-      let address = (address as u32) & 0xFFFF;
-      let value = (value as u32) & 0xFFFF;
-      memory.lock().unwrap().insert(address, value as u16);
-    }
-  });
-  let extmem_store_8_func = Func::wrap(&store, {
-    let memory = memory.clone();
-    move |address: i32, value: i32| -> () {
-      let address = (address as u32) & 0xFFFF;
-      let value = (value as u32) & 0xFFFF;
-      memory.lock().unwrap().insert(address, value as u8 as u16);
-    }
-  });
-
-  /*
-  (type $t0 (func))
-  (type $t1 (func (param i32 i32)))
-  (type $t2 (func (param i32) (result i32)))
-  (type $t3 (func (param i32)))
-  (type $t4 (func (result i32)))
-
-  (import "root" "print" (func $print (type $t2)))
-  (import "root" "extmem_load" (func $extmem_load (param i32) (result i32)))
-  (import "root" "extmem_load_8" (func $extmem_load_8 (param i32) (result i32)))
-  (import "root" "extmem_store" (func $extmem_store (param i32) (param i32)))
-  (import "root" "extmem_store_8" (func $extmem_store_8 (param i32) (param i32)))
-  */
-  let imports = [
-    print_func.into(),
-    extmem_load_func.into(),
-    extmem_load_8_func.into(),
-    extmem_store_func.into(),
-    extmem_store_8_func.into(),
-  ];
-
-  // Once we've got that all set up we can then move to the instantiation
-  // phase, pairing together a compiled module as well as a set of imports.
-  // Note that this is where the wasm `start` function, if any, would run.
-  let instance = Instance::new(&module, &imports)?;
-
-  // Next we poke around a bit to extract the `run` function from the module.
-  let run = instance
-    .get_export("main")
-    .and_then(|e| e.func())
-    .ok_or(anyhow::format_err!("failed to find `run` function export"))?
-    .get0::<i32>()?;
-
-  run()?;
-
-  let buffer_ref = buffer.lock().unwrap();
-  Ok(buffer_ref.iter().cloned().collect::<Vec<u8>>())
+    let mut runner = parse_wasm(binary, export_buffer)?;
+    runner.run()?;
+    Ok(runner.flush_buffer()?)
 }
