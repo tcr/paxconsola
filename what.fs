c014a8d207a199384fcb0a09dbaae7cdbfb620e0
Tim Ryan <trim@fb.com>
2021-06-24 22:35:02 -0400

    compare

---
+++ what.fs
@@ -0,0 +1,10 @@
+s" " s" " compare .
+s" abdde" s" abdde" compare .
+s" abcde" s" abdde" compare .
+s" abdde" s" abcde" compare .
+s" abcde" s" abcdef" compare .
+s" abcdef" s" abcde" compare .
+s" abc" s" aBc" compare .
+s" aBc" s" abc" compare .
+
+bye
