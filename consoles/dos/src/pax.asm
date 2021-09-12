1fc49abbf58cbb7ca3131a4488605c8016e33ad4
Tim Ryan <id@timryan.org>
2021-09-12 00:02:57 -0400

    Start breaking out engines

---
+++ consoles/dos/src/pax.asm
@@ -0,0 +1,170 @@
+[map symbols build/paxconsola.map]
+
+%define DosEGA          1
+
+    section .text
+
+    org 100h
+	
+	mov ax, $0		;ES points to our Code segment
+    mov es, ax
+
+	call ScreenInit
+
+    push ds
+    push es
+
+        ; Setup the palette
+        mov si,Palette
+        xor ax,ax
+Paletteagain:
+        mov dx,[si]
+        call SetPalette ;Set Color AL to DX (-GRB)
+        inc si			;Move down two bytes
+        inc si
+        inc ax
+        cmp ax,16		;Are we done?
+        jnz Paletteagain
+
+        ; https://www.vcfed.org/forum/forum/technical-support/vintage-computer-programming/56910-vga-cga-graphics-library-for-dos-games
+
+        ; Setup return stack for Forth
+        mov bx,0xF000
+        mov bp,bx
+
+        ; Call Forth
+        xchg sp,bp
+        call PAX_FN_main
+        xchg sp,bp
+
+        ; Extract values from Forth
+        mov ax, bx ; move TOS
+        mov bh, al
+        pop ax
+        mov bl, al
+        pop ax ; get rid of first value
+
+        ;mov bh,6	;X
+        ;mov bl,11	;Y
+
+        ; bh = X
+        ; bl = Y
+        call GetScreenPos
+        ; es:di = destination bitplane
+
+        %ifdef DosCGA
+            mov ch,12	;Width
+        %endif
+        %ifdef DosEGA
+            mov ch,6	;Width
+        %endif
+        %ifdef DosVGA
+            mov ch,48	;Width
+        %endif
+        mov cl,48		;Height
+        mov si, BitmapTest 
+        ; bh = X
+        ; bl = Y
+        ; ch = Width
+        ; cl = Height
+        ; ds:si = source bitmap
+        ; es:di = destination bitplane
+
+DrawBitmap_Yagain:
+        push di
+        push cx
+DrawBitmap_Xagain:
+            %ifdef DosEGA
+                mov ax,0102h	;plane 0 (0102h)
+                mov dx,03c4h		
+                out dx,ax		;Apply plane mask
+                movsb			;DS:SI -> ES:DI
+                dec di			;Reset Dest Ram
+                
+                mov ah,2h		;plane 1 (0202h)
+                out dx,ax		;Apply plane mask
+                movsb			;DS:SI -> ES:DI
+                dec di			;Reset Dest Ram
+                
+                mov ah,4h		;plane 2 (0402h)
+                out dx,ax		;Apply plane mask
+                movsb			;DS:SI -> ES:DI
+                dec di			;Reset Dest Ram
+                
+                mov ah,8h		;plane 3 (0802h)
+                out dx,ax		;Apply plane mask
+                movsb			;DS:SI -> ES:DI
+            %endif
+            %ifdef DosCGA
+                movsb			;DS:SI -> ES:DI
+            %endif
+            %ifdef DosVGA
+                movsb				;DS:SI -> ES:DI
+            %endif
+            dec ch
+            jnz DrawBitmap_Xagain
+        pop cx
+        pop di
+        call GetScreenNextLine
+        inc bl
+        dec cl
+        jnz DrawBitmap_Yagain
+    pop es
+    pop ds
+	
+	jmp $
+	
+    ; Forth compilation
+%include "build/paxconsola_generated.asm"
+
+    ; Pop return address
+    xchg sp,bp
+    ret
+
+    align 16
+%include "src/bitmap.asm"
+
+    align 16
+BitmapFont:
+	incbin "lib/Font96.FNT"
+BitmapFontEnd:
+	
+    align 16
+BitmapTest:	
+	; incbin "lib/SpriteTestEGA.RAW"
+    incbin "build/tiles.raw"
+BitmapTestEnd:
+
+
+Palette:
+	dw 0250h;0  -GRB 
+	dw 0000h;1  -GRB
+	dw 0555h;2  -GRB
+	dw 0AAAh;3  -GRB
+	dw 0FFFh;4  -GRB
+	dw 0826h;5  -GRB
+	dw 0D33h;6  -GRB
+	dw 03E3h;7  -GRB
+	dw 07E6h;8  -GRB
+	dw 0AE5h;9  -GRB
+	dw 0FF4h;10  -GRB
+	dw 02AAh;11  -GRB
+	dw 00FFh;12  -GRB
+	dw 030Dh;13  -GRB
+	dw 063Bh;14  -GRB
+	dw 0D0Fh;15  -GRB
+
+
+    section .data
+
+UserRam times 256 db 0
+	
+CursorX			equ UserRam
+CursorY			equ UserRam+1
+MonitorBak_AX 	equ UserRam+2
+MonitorBak_F	equ UserRam+4
+MonitorBak_IP   equ UserRam+6
+MonitorBak_ES   equ UserRam+8
+MonitorBak_DS   equ UserRam+10
+
+; EXE_end
