ce3e02a0fd8e6b55340ba0508eb57e19742cfee6
Tim Ryan <id@timryan.org>
2021-09-30 01:40:09 -0400

    [ide] Publish mdbook

--- paxconsola-ide/build-all.sh
+++ paxconsola-ide/build-all.sh
@@ -2,7 +2,11 @@
 
 set -x
 
+# For netlify
 rustup toolchain install stable
+rustup target add wasm32-unknown-unknown
+
 cargo install trunk
+(cd ../doc; mdbook build; )
 ./compile-workers.sh
 trunk build
