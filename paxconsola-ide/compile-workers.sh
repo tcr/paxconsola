f0bfe7215a708bd46ae3ba0e1d6536783a08b2ed
Tim Ryan <id@timryan.org>
2021-09-30 12:27:29 -0400

    [ide] Speeds up CI build.

--- paxconsola-ide/compile-workers.sh
+++ paxconsola-ide/compile-workers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-set -e
+# set -e
 
-cargo build --target=wasm32-unknown-unknown --bin compilation_worker --release
-mkdir -p target/workers/
-wasm-bindgen --target no-modules ./target/wasm32-unknown-unknown/release/compilation_worker.wasm \
-    --out-dir target/workers/
+cargo build --target=wasm32-unknown-unknown --bin compilation_worker
+mkdir -p ../target/workers/
+wasm-bindgen --target no-modules ../target/wasm32-unknown-unknown/debug/compilation_worker.wasm \
+    --out-dir ../target/workers/
