4efa54631ffaa38c0a3113e71aa1e8038a741215
Tim Ryan <id@timryan.org>
2021-09-30 02:20:28 -0400

    [ide] mdbook build

--- paxconsola-ide/build-all.sh
+++ paxconsola-ide/build-all.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 set -x
+set -e
 
 # For netlify
-rustup toolchain install stable
 rustup target add wasm32-unknown-unknown
 
-cargo install trunk
+VERSION="v0.14.0"
+wget -qO- https://github.com/thedodd/trunk/releases/download/${VERSION}/trunk-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
+wget -qO- https://github.com/rust-lang/mdBook/releases/download/v0.4.12/mdbook-v0.4.12-x86_64-unknown-linux-gnu.tar.gz | tar -xzf-
+wget -qO- https://github.com/rustwasm/wasm-bindgen/releases/download/0.2.78/wasm-bindgen-0.2.78-x86_64-unknown-linux-musl.tar.gz | tar -xzf-
+mv wasm-bindgen-0.2.78-x86_64-unknown-linux-musl/wasm-bindgen .
+
+export PATH=$(pwd):$PATH
+
 (cd ../doc; mdbook build; )
 ./compile-workers.sh
-trunk build
+./trunk build
